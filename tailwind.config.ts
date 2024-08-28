8ab49ec5c95a7e9dc1fd00478e2a0ac796f16b35
M-Shourav <muhammadshourav625@gmail.com>
2024-08-28 10:12:27 +0600

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { Poppins } from "next/font/google";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -8,10 +9,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        bodyFont: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        themeColor: "#030D25",
       },
     },
   },
