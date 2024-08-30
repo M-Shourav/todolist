f1e26d534474dcd5d218d1344b2e2514905109db
M-Shourav <muhammadshourav625@gmail.com>
2024-08-30 07:16:44 +0600

    keys

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,17 @@ import { MdOutlineMailOutline } from "react-icons/md";
 const Fotter = () => {
   const footerArray = [
     {
+      _id: 1,
       icon: <FaGithub />,
       href: "https://github.com/M-Shourav",
     },
     {
+      _id: 2,
       icon: <FaFacebookF />,
       href: "https://www.facebook.com/profile.php?id=100027878711748",
     },
     {
+      _id: 3,
       icon: <MdOutlineMailOutline />,
       href: "https://www.facebook.com/profile.php?id=100027878711748",
     },
@@ -31,7 +34,7 @@ const Fotter = () => {
       >
         {footerArray?.map((item) => (
           <Link
-            key={item?.href}
+            key={item?._id}
             href={item?.href}
             target="blank"
             className="w-10 h-10 flex items-center justify-center rounded-full border border-gray-600
